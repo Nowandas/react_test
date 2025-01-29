062d278e470ef645037650fae3e5322ec5bf5e74
Nowandas <anapliotisnick@gmail.com>
2025-01-29 15:46:32 +0200

    brad tutorial 34 min mark

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,18 @@
 const App = () => {
+  const email='anapliotisnick@gmail.com'
+  const x=35
+  const names=['Nick','Anna','John']
+  
   return (
-    <div>anapliotisnick@gmail.com</div>
+    <div>
+      <div>Hello, {email}</div>
+      <p>You are {x} years old.</p>
+      <ul>
+        {names.map(name => (
+          <li key={name}>{name}</li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
