9f00d0a0b6dc504a7b59457269fe4ae2c28de68f
Nowandas <anapliotisnick@gmail.com>
2025-01-30 16:22:55 +0200

    add job form under construction

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,14 @@ import JobsPage from './pages/JobsPage'
 import NotFoundPage from './pages/NotFoundPage'
 import JobPage from './pages/JobPage'
 import { jobLoader } from './loaders/JobLoader'
+import AddJobPage from './pages/AddJobPage'
 
 const router = createBrowserRouter(
   createRoutesFromElements([
     <Route path="/" element={<MainLayout />} key="main-layout">
       <Route index element={<HomePage />} key="home-page" />,
       <Route path='/jobs' element={<JobsPage />} key="jobs-page" />,
+      <Route path='/add-job' element={<AddJobPage />} key="add-job-page" />,
       <Route path='/jobs/:id' element={<JobPage />} loader={jobLoader} key="job-page" />,
       <Route path='*' element={<NotFoundPage />} key="not-found-page" />,
     </Route>
